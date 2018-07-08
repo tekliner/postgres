0fe121e9751fded8362ae0a42ebaaa1a9cc6d348
Tamal Saha <tamal@appscode.com>
2018-07-08 11:25:35 -0700

    Format shell scripts (#153)
    find hack | grep sh | xargs shfmt -l -w -ci -i 2 {}

--- hack/docker/postgres/10.2/make.sh
+++ hack/docker/postgres/10.2/make.sh
@@ -16,34 +16,34 @@ DIST="$REPO_ROOT/dist"
 mkdir -p "$DIST"
 
 build_binary() {
-    pushd $REPO_ROOT
-    ./hack/builddeps.sh
-    ./hack/make.py build pg-operator
-    popd
+  pushd $REPO_ROOT
+  ./hack/builddeps.sh
+  ./hack/make.py build pg-operator
+  popd
 }
 
 build_docker() {
-    pushd "$REPO_ROOT/hack/docker/postgres/$TAG"
+  pushd "$REPO_ROOT/hack/docker/postgres/$TAG"
 
-    # Download wal-g
-    wget https://github.com/kubedb/wal-g/releases/download/${WALG_VER}/wal-g-alpine-amd64
-    chmod +x wal-g-alpine-amd64
-    mv wal-g-alpine-amd64 wal-g
+  # Download wal-g
+  wget https://github.com/kubedb/wal-g/releases/download/${WALG_VER}/wal-g-alpine-amd64
+  chmod +x wal-g-alpine-amd64
+  mv wal-g-alpine-amd64 wal-g
 
-    # Copy pg-operator
-    cp "$DIST/pg-operator/pg-operator-alpine-amd64" pg-operator
-    chmod 755 pg-operator
+  # Copy pg-operator
+  cp "$DIST/pg-operator/pg-operator-alpine-amd64" pg-operator
+  chmod 755 pg-operator
 
-    local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
-    echo $cmd; $cmd
+  local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
+  echo $cmd; $cmd
 
-    rm wal-g pg-operator
-    popd
+  rm wal-g pg-operator
+  popd
 }
 
 build() {
-    build_binary
-    build_docker
+  build_binary
+  build_docker
 }
 
 binary_repo $@
