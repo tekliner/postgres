0b09ecc6220594384fd71c551c810fd3323d0ae7
Vladimir Smagin <21h@blindage.org>
2018-11-13 15:19:23 +0700

    13.11.2018

--- pkg/leader_election/leader_election.go
+++ pkg/leader_election/leader_election.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"os/user"
-	"strconv"
 	"strings"
 	"time"
 
@@ -31,8 +29,39 @@ const (
 	RoleReplica = "replica"
 )
 
+type pgOpCommand int
+
+const (
+	startMasterEmpty = iota
+	startMasterRecovery
+	startSlave
+	createRecoveryTrigger
+	removeRecoveryTrigger
+	promoteToMaster
+	promoteToSlave
+	raiseError
+	raiseFatalError
+)
+
+type pgOpErrorType int
+
+const (
+	masterUnreachable = iota
+	masterNotFunctional
+	backupUnreachable
+	lostSync
+	walgError
+	noLeader
+)
+
+type pgOpError struct {
+	ErrorType pgOpErrorType
+	ErrorText string
+}
+
 func appendFile(filename string, lines []string) error {
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+	log.Printf("appendFile: %s", filename)
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		return err
 	}
@@ -47,9 +76,27 @@ func appendFile(filename string, lines []string) error {
 }
 
 func dataDirectoryCleanup() {
+	log.Println("dataDirectoryCleanup: Removing and creating data directory")
 	PGDATA := getEnv("PGDATA", "/var/pv/data")
 	os.RemoveAll(PGDATA)
-	os.MkdirAll(PGDATA, 0700)
+	os.MkdirAll(PGDATA, 0755)
+	setPermission()
+}
+
+func dataDirectoryCreateAfterWalg() {
+	log.Println("dataDirectoryCreateAfterWalg: creating directories after wal-g")
+	PGDATA := getEnv("PGDATA", "/var/pv/data")
+	dirList := []string{
+		"/pg_tblspc",
+		"/pg_twophase",
+		"/pg_stat",
+		"/pg_commit_ts",
+		"/pg_logical/snapshots",
+		"/pg_logical/mappings",
+	}
+	for _, dir := range dirList {
+		os.MkdirAll(PGDATA+dir, 0700)
+	}
 }
 
 func getEnv(key, fallback string) string {
@@ -93,11 +140,20 @@ func setPosgresUserPassword(username, password string) {
 	}
 }
 
-func isPostgresOnline(hostname string, wait bool) bool {
+func isPostgresOnline(ctx context.Context, hostname string, wait bool) bool {
 	// authung! dangerous function
 	//if wait == true function will wait until connection established
 	returnValue := false
-	for {
+	exitLoop := false
+	for exitLoop == false {
+		select {
+		case <-ctx.Done():
+			exitLoop = true
+			returnValue = false
+			break
+		default:
+		}
+
 		log.Println("isPgMasterOnline: Checking connection to master")
 
 		if db, err := sql.Open("postgres", pgConnString(hostname)); db != nil {
@@ -115,12 +171,12 @@ func isPostgresOnline(hostname string, wait bool) bool {
 		if !wait {
 			break
 		}
-		time.Sleep(time.Second * 5)
+		time.Sleep(time.Second * 60)
 	}
 	return returnValue
 }
 
-func runCmd(env []string, cmdname string, params ...string) {
+func runCmd(env []string, cmdname string, params ...string) bool {
 	log.Println("runCmd:", cmdname, params)
 	cmd := exec.Command(cmdname, params...)
 
@@ -128,6 +184,7 @@ func runCmd(env []string, cmdname string, params ...string) {
 	cmd.Env = append(cmd.Env, os.Environ()...)
 	cmd.Env = append(cmd.Env, env...)
 
+	log.Printf("runCmd: env %v", env)
 	// set stdout, stderr
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -135,11 +192,12 @@ func runCmd(env []string, cmdname string, params ...string) {
 	// if errors
 	if err := cmd.Run(); err != nil {
 		log.Println(err)
+		return false
 	}
-	os.Exit(1)
+	return true
 }
 
-func execWalgAction(walgCommand string, params ...string) {
+func execWalgAction(walgCommand string, params ...string) bool {
 	var env []string
 	env = append(env, fmt.Sprintf("WALE_S3_PREFIX=%s", getEnv("ARCHIVE_S3_PREFIX", "")))
 	// auth for wal-g
@@ -163,33 +221,61 @@ func execWalgAction(walgCommand string, params ...string) {
 	env = append(env, fmt.Sprintf("AWS_ACCESS_KEY_ID=%s", awsKey))
 	env = append(env, fmt.Sprintf("AWS_SECRET_ACCESS_KEY=%s", awsSecret))
 	// need to forward "wal-g", walgCommand, params...
-	arg := []string{"postgres", "wal-g"}
+	arg := []string{"root", "wal-g"}
 	arg = append(arg, walgCommand)
 	arg = append(arg, params...)
-	runCmd(env, "su-exec", arg...)
+	return runCmd(env, "su-exec", arg...)
 }
 
-func execBaseBackup() {
+func execBaseBackup() bool {
+	log.Println("execBaseBackup: running pg_basebackup")
 	var env []string
+	env = append(env, fmt.Sprintf("PGUSER=%s", getEnv("POSTGRES_USER", "postgres")))
+	env = append(env, fmt.Sprintf("PGPASSWORD=%s", getEnv("POSTGRES_PASSWORD", "postgres")))
 	pgdata := getEnv("PGDaTA", "/var/pv/data")
-	pguser := fmt.Sprintf("--username=%s", getEnv("POSTGRES_USER", "postgres"))
 	pghost := fmt.Sprintf("--host=%s", getEnv("PRIMARY_HOST", ""))
-	runCmd(env, "pg_basebackup", "-X", "fetch", "--no-password", "--pgdata", pgdata, pguser, pghost)
+	return runCmd(env, "pg_basebackup", "-X", "fetch", "--no-password", "--pgdata", pgdata, pghost)
 }
 
 func execPostgresAction(action string) {
+	log.Printf("execPostgresAction: %s", action)
 	var env []string
+	env = append(env, fmt.Sprintf("WALE_S3_PREFIX=%s", getEnv("ARCHIVE_S3_PREFIX", "")))
+	// auth for wal-g
+	env = append(env, fmt.Sprintf("PGUSER=%s", getEnv("POSTGRES_USER", "")))
+	env = append(env, fmt.Sprintf("PGPASSWORD=%s", getEnv("POSTGRES_PASSWORD", "")))
+
+	awsKeyFile := "/srv/wal-g/archive/secrets/AWS_ACCESS_KEY_ID"
+	awsSecretFile := "/srv/wal-g/archive/secrets/AWS_SECRET_ACCESS_KEY"
+
+	awsKey, err := ioutil.ReadFile(awsKeyFile)
+	// aws key file ansent
+	if err != nil {
+		log.Println(err)
+	}
+
+	awsSecret, err := ioutil.ReadFile(awsSecretFile)
+	// aws secret file absent
+	if err != nil {
+		log.Println(err)
+	}
+	env = append(env, fmt.Sprintf("AWS_ACCESS_KEY_ID=%s", awsKey))
+	env = append(env, fmt.Sprintf("AWS_SECRET_ACCESS_KEY=%s", awsSecret))
 	runCmd(env, "su-exec", "postgres", "pg_ctl", "-D", getEnv("PGDaTA", "/var/pv/data"), "-w", action)
 }
 
 func postgresMakeEmptyDB() {
-	// Create empty database for postgres after dir cleanup and before start
+	log.Println("postgresMakeEmptyDB: Create empty database for postgres")
 	var env []string
 	runCmd(env, "initdb", fmt.Sprintf("--pgdata=%s", getEnv("PGDATA", "/var/pv/data")))
 }
 
+func postgresInitDB() {
+	// TODO: Search INITDB directory for init files
+}
+
 func postgresMakeConfigs(role string) {
-	// Create config files for postgres after dir cleanup and before start
+	log.Printf("Create config files for postgres, role: %s", role)
 	if role == RolePrimary {
 		var env []string
 		// copy template to /tmp
@@ -200,6 +286,16 @@ func postgresMakeConfigs(role string) {
 			"wal_level = replica",
 			"max_wal_senders = 99",
 			"wal_keep_segments = 32",
+		}
+		if getEnv("STREAMING", "") == "synchronous" {
+			// setup synchronous streaming replication
+			lines = append(lines, "synchronous_commit = remote_write")
+			lines = append(lines, "synchronous_standby_names = '*'")
+		}
+		if getEnv("ARCHIVE", "") == "wal-g" {
+			lines = append(lines, "archive_command = 'wal-g wal-push %p'")
+			lines = append(lines, "archive_timeout = 60")
+			lines = append(lines, "archive_mode = always")
 		}
 		appendFile("/tmp/postgresql.conf", lines)
 
@@ -249,6 +345,122 @@ func postgresMakeConfigs(role string) {
 	}
 }
 
+func masterLoop(ctx context.Context, commandsBus chan pgOpCommand, recoverySuccessful chan bool) {
+	log.Println("master loop welcomes you")
+	exitLoop := false
+	for exitLoop == false {
+		select {
+		case <-ctx.Done():
+			exitLoop = true
+			break
+		case operatorCommand := <-commandsBus:
+			// receive message
+			if operatorCommand == startMasterEmpty {
+				log.Println("master loop: Received command start as master:", operatorCommand)
+				// some actions before start as master
+				dataDirectoryCleanup()
+				postgresMakeConfigs(RolePrimary)
+				postgresMakeEmptyDB()
+				setPermission()
+				execPostgresAction("start")
+			}
+			if operatorCommand == startMasterRecovery {
+				log.Println("master loop: Received command start recovery:", operatorCommand)
+				// absolutely clean data directory
+				dataDirectoryCleanup()
+
+				// some actions to start recovery
+				execWalgAction("backup-list")
+				restoreComplete := execWalgAction("backup-fetch", getEnv("PGDaTA", "/var/pv/data"), getEnv("BACKUP_NAME", "LATEST"))
+				dataDirectoryCreateAfterWalg()
+				postgresMakeConfigs(RolePrimary)
+
+				// additional lines to recovery.conf
+				lines := []string{}
+				if getEnv("PITR", "") != "" {
+
+					target_inclusive := getEnv("TARGET_INCLUSIVE", "true")
+					target_time := getEnv("TARGET_TIME", "")
+					target_timeline := getEnv("TARGET_TIMELINE", "")
+					target_xid := getEnv("TARGET_XID", "")
+
+					lines = []string{
+						fmt.Sprintf("recovery_target_inclusive = '%s'", target_inclusive),
+						"recovery_target_action = 'promote'",
+					}
+					if target_time != "" {
+						lines = append(lines, fmt.Sprintf("recovery_target_time = '%s'", target_time))
+					}
+					if target_timeline != "" {
+						lines = append(lines, fmt.Sprintf("recovery_target_timeline = '%s'", target_timeline))
+					}
+					if target_xid != "" {
+						lines = append(lines, fmt.Sprintf("recovery_target_xid = '%s'", target_xid))
+					}
+
+				}
+				lines = append(lines, "restore_command = 'wal-g wal-fetch %f %p'")
+				appendFile(getEnv("PGDATA", "/var/pv/data")+"/recovery.conf", lines)
+				os.Remove(getEnv("PGDATA", "/var/pv/data") + "/recovery.done")
+				setPermission()
+				go execPostgresAction("start")
+				// backup done, start Postgres
+				if restoreComplete == false {
+					recoverySuccessful <- true
+				} else {
+					recoverySuccessful <- false
+				}
+				// if alien backup used set user and password to current deployment credentials
+				// be sure that current deployment role have rights to all required resources
+				ctxMasterRecovery, cancelMasterRecovery := context.WithCancel(ctx)
+				defer cancelMasterRecovery()
+				if isPostgresOnline(ctxMasterRecovery, "localhost", true) {
+					appendFile("/tmp/pg-failover-trigger", []string{})
+					recovery_done_file := getEnv("PGDATA", "/var/pv/data") + "/recovery.done"
+					for {
+						if _, err := os.Stat(recovery_done_file); !os.IsNotExist(err) {
+							break
+						}
+						log.Println("master loop: Waiting recovery.done to be created")
+						time.Sleep(time.Second)
+					}
+					setPosgresUserPassword(getEnv("POSTGRES_USER", "postgres"), getEnv("POSTGRES_PASSWORD", "postgres"))
+				}
+
+			}
+			if operatorCommand == startSlave {
+				ctxSlave, cancelSlave := context.WithCancel(ctx)
+				defer cancelSlave()
+				if isPostgresOnline(ctxSlave, getEnv("PRIMARY_HOST", ""), true) {
+					log.Println("master loop: Received command start as slave:", operatorCommand)
+					// some actions before start as slave
+					dataDirectoryCleanup()
+					restoreComplete := execBaseBackup()
+					postgresMakeConfigs(RoleReplica)
+					setPermission()
+					if restoreComplete == false {
+						recoverySuccessful <- true
+					} else {
+						recoverySuccessful <- false
+					}
+					execPostgresAction("start")
+				}
+			}
+			if operatorCommand == createRecoveryTrigger {
+				log.Println("master loop: Received command create failover trigger:", operatorCommand)
+
+				if !ioutil.WriteString("/tmp/pg-failover-trigger", "") {
+					log.Println("master loop: Failed to create trigger file")
+				}
+			}
+
+		default:
+			// nothing, sleep to lower cpu usage
+			time.Sleep(time.Second)
+		}
+	}
+}
+
 func RunLeaderElection() {
 
 	log.Println("Leader election started")
@@ -261,13 +473,13 @@ func RunLeaderElection() {
 	}
 
 	// Change owner of Postgres data directory
-	if err := setPermission(); err != nil {
-		log.Fatalln(err)
+	if setPermission() == false {
+		log.Println("RunLeaderElection: can't chown data directory")
 	}
 
 	hostname, err := os.Hostname()
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
 	parts := strings.Split(hostname, "-")
@@ -277,13 +489,13 @@ func RunLeaderElection() {
 
 	config, err := restclient.InClusterConfig()
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	clientcmd.Fix(config)
 
 	kubeClient, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
 	configMap := &core.ConfigMap{
@@ -293,7 +505,7 @@ func RunLeaderElection() {
 		},
 	}
 	if _, err := kubeClient.CoreV1().ConfigMaps(namespace).Create(configMap); err != nil && !kerr.IsAlreadyExists(err) {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
 	resLock := &resourcelock.ConfigMapLock{
@@ -305,97 +517,13 @@ func RunLeaderElection() {
 		},
 	}
 
-	type pgOpCommand int
-	const (
-		startMasterEmpty      pgOpCommand = 0
-		startMasterRecovery   pgOpCommand = 1
-		startSlave            pgOpCommand = 2
-		createRecoveryTrigger pgOpCommand = 3
-		removeRecoveryTrigger pgOpCommand = 4
-		promoteToMaster       pgOpCommand = 5
-		promoteToSlave        pgOpCommand = 6
-		raiseError            pgOpCommand = 7
-		raiseFatalError       pgOpCommand = 8
-	)
-
-	type pgOpErrorType int
-	const (
-		masterUnreachable   pgOpErrorType = 0
-		masterNotFunctional pgOpErrorType = 1
-		backupUnreachable   pgOpErrorType = 2
-		lostSync            pgOpErrorType = 3
-		walgError           pgOpErrorType = 4
-		noLeader            pgOpErrorType = 5
-	)
-
-	type pgOpError struct {
-		ErrorType pgOpErrorType
-		ErrorText string
-	}
-
-	//messagesBus := make(chan string)
 	commandsBus := make(chan pgOpCommand)
-	errorsBus := make(chan pgOpError)
+	recoverySuccessful := make(chan bool)
 	runningFirstTime := true
-
-	go func() {
-		// master loop
-		for {
-			select {
-			case operatorCommand := <-commandsBus:
-				// receive message
-				if operatorCommand == startMasterEmpty {
-					log.Println("master loop: Received command start as master:", operatorCommand)
-					// some actions before start as master
-					dataDirectoryCleanup()
-					postgresMakeConfigs(RolePrimary)
-					postgresMakeEmptyDB()
-					execPostgresAction("start")
-				}
-				if operatorCommand == startMasterRecovery {
-					log.Println("master loop: Received command start recovery:", operatorCommand)
-					// some actions before start recovery
-					dataDirectoryCleanup()
-					postgresMakeConfigs(RolePrimary)
-					// some actions to start recovery
-					execWalgAction("backup-list")
-					execWalgAction("backup-fetch", getEnv("PGDaTA", "/var/pv/data"), getEnv("BACKUP_NAME", "LATEST"))
-					setPermission()
-					// backup done, start Postgres
-					go execPostgresAction("start")
-					// if alien backup used set user and password to current deployment credentials
-					// be sure that current deployment role have rights to all required resources
-					if isPostgresOnline("localhost", true) {
-						setPosgresUserPassword(getEnv("POSTGRES_USER", "postgres"), getEnv("POSTGRES_PASSWORD", "postgres"))
-					}
-
-				}
-				if operatorCommand == startSlave {
-					log.Println("master loop: Received command start as slave:", operatorCommand)
-					// some actions before start as slave
-					dataDirectoryCleanup()
-					execBaseBackup()
-					postgresMakeConfigs(RoleReplica)
-					setPermission()
-					execPostgresAction("start")
-				}
-				if operatorCommand == createRecoveryTrigger {
-					log.Println("master loop: Received command create failover trigger:", operatorCommand)
-
-					if !ioutil.WriteString("/tmp/pg-failover-trigger", "") {
-						log.Fatalln("master loop: Failed to create trigger file")
-					}
-				}
-			case operatorError := <-errorsBus:
-				// TODO: create exceptions processing
-				log.Printf("master loop: Got error %v", operatorError)
-
-			default:
-				// nothing, sleep to lower cpu usage
-				time.Sleep(time.Second)
-			}
-		}
-	}()
+	databaseRestored := false
+	mlCtx, mlCancel := context.WithCancel(context.Background())
+	go masterLoop(mlCtx, commandsBus, recoverySuccessful)
+	defer mlCancel()
 
 	go func() {
 		leaderelection.RunOrDie(context.Background(), leaderelection.LeaderElectionConfig{
@@ -415,14 +543,14 @@ func RunLeaderElection() {
 					log.Printf("RunOrDie: We got new leader - %v!", identity)
 					statefulSet, err := kubeClient.AppsV1().StatefulSets(namespace).Get(statefulSetName, metav1.GetOptions{})
 					if err != nil {
-						log.Fatalln(err)
+						log.Println(err)
 					}
 
 					pods, err := kubeClient.CoreV1().Pods(namespace).List(metav1.ListOptions{
 						LabelSelector: metav1.FormatLabelSelector(statefulSet.Spec.Selector),
 					})
 					if err != nil {
-						log.Fatalln(err)
+						log.Println(err)
 					}
 
 					for _, pod := range pods.Items {
@@ -444,7 +572,15 @@ func RunLeaderElection() {
 							// OS env variable RESTORE contains true or false
 							if getEnv("RESTORE", "false") == "true" {
 								log.Println("RunOrDie: $RESTORE is true, sending signal to start recovery")
+								// create context with cancel go master loop go routine
 								commandsBus <- startMasterRecovery
+								recoveryComplete := <-recoverySuccessful
+								if recoveryComplete == true {
+									databaseRestored = true
+								} else {
+									databaseRestored = false
+								}
+
 							} else {
 								log.Println("RunOrDie: $RESTORE is false, sending signal to start empty master")
 								commandsBus <- startMasterEmpty
@@ -452,12 +588,28 @@ func RunLeaderElection() {
 						} else {
 							log.Println("RunOrDie: Pod started as slave server")
 							commandsBus <- startSlave
+							recoveryComplete := <-recoverySuccessful
+							if recoveryComplete == true {
+								databaseRestored = true
+							} else {
+								databaseRestored = false
+							}
 						}
 
 					} else {
-						log.Println("RunOrDie: Pod NOT started first time")
-						if identity == hostname {
-							commandsBus <- createRecoveryTrigger
+						mlCancel()
+						go masterLoop(mlCtx, commandsBus, recoverySuccessful)
+						if databaseRestored == true {
+							if identity == hostname {
+								log.Println("RunOrDie: Creating recovery trigger")
+								commandsBus <- createRecoveryTrigger
+							}
+						} else {
+							if identity == hostname {
+								log.Println("RunOrDie: Database not restored! Cant promote slave to master")
+								log.Println("RunOrDie: Trying to restore database from backup")
+								commandsBus <- startMasterRecovery
+							}
 						}
 					}
 
@@ -469,24 +621,12 @@ func RunLeaderElection() {
 	select {}
 }
 
-func setPermission() error {
-	u, err := user.Lookup("postgres")
-	if err != nil {
-		return err
-	}
-	uid, err := strconv.Atoi(u.Uid)
-	if err != nil {
-		return err
-	}
-	gid, err := strconv.Atoi(u.Gid)
-	if err != nil {
-		return err
-	}
-	err = os.Chown("/var/pv", uid, gid)
-	if err != nil {
-		return err
-	}
-	return nil
+func setPermission() bool {
+	log.Println("setPermission: chown data directory")
+	var env []string
+	runCmd(env, "chown", "-R", "postgres:postgres", getEnv("PGDaTA", "/var/pv/data"))
+	runCmd(env, "chmod", "-R", "700", getEnv("PGDaTA", "/var/pv/data"))
+	return true
 }
 
 func GetLeaderLockName(offshootName string) string {
